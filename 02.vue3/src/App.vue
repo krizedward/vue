a50ebc3e70a322b5b4a4815252e025da0c54aba3
krizedward <edward.mangare@binus.ac.id>
2023-01-11 17:35:48 +0700

    step-05

---
+++ 02.vue3/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="product.length">
+  <ul>
+    <li v-for="item in product" :key="item.id">
+      {{ item.title }} - {{  item.price }}
+    </li>
+  </ul>
+  </div>
+  
+  <div v-else>
+    <p>No Data Found</p>
+  </div>
+  
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      nama: "",
+      product:[
+        {id: 1, title: "product 1", price:3000},
+        {id: 2, title: "product 2", price:3000},
+        {id: 3, title: "product 3", price:4000},
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 02.vue3/src/components/Dokumentasi.vue
@@ -0,0 +1,43 @@
+<template>
+    <h2>Welcome</h2>
+    <h1>{{ nama }}</h1>
+    <button @click="changeName">Change Name</button>
+    <!-- memanggil link -->
+    <a :href="url">Go Web</a>
+    <!-- two way data binding -->
+    <input type="text" v-model="name" />
+    {{ name }}
+</template>
+
+<script>
+
+export default {
+    name: 'App',
+    data() {
+        return {
+            nama: "Edward Kristian Mangare",
+            // data binding
+            url: "https://nihonuwu.com",
+            // two way data binding
+            name: "",
+
+        }
+    },
+    methods: {
+        changeName: function () {
+            this.nama = "Heru"
+        }
+    },
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
